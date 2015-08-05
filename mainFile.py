a2b4ed40b79344553f039581f82a08055a14dca5
Alex Ashton <a.ashton@hotmail.co.uk>
2015-08-05 12:30:57 +0100

    Continued Work
    - Some bug fixed
    - more suedo code in plan
    - added more files to organise code

--- mainFile.py
+++ mainFile.py
@@ -1,27 +1,87 @@
 import csv
 import matplotlib
 from shapeComparison import comparePattern
+from shapeComparison import percentageChange
 from dataProcessing import makeHistoricData
+from matplotlib import pyplot
+from functools import reduce
 
-currentPattern = []
+
 historicData = makeHistoricData()
+pastPatterns = []
+currentPattern = historicData[-300:-285]
+print('Current Pattern : ' + str(currentPattern))
+historicData = historicData
+futureDistance = 10
 
 def generatePastPatterns(patternLength, historicData):
     currentPoint = 0
-    endPoint = len(historicData) - patternLength
+    endPoint = len(historicData) - patternLength - futureDistance
     pastPatterns = []
     while currentPoint < endPoint:
+        #print('Current Point ' +str(currentPoint))
         x = 0
         patternToAdd = []
-        while x < patternLength:
+        while x < patternLength + futureDistance:
             patternToAdd.append(historicData[currentPoint + x])
+            #print('Data item to add' + str(historicData[currentPoint + x]))
             x += 1
+            #print('x = ' + str(x))
         pastPatterns.append([patternToAdd,currentPoint])
+        #print('Pattern to Add :' + str(patternToAdd))
+        currentPoint += 1
     return pastPatterns
 
+def graphSimilar():
+    xList = []
+    count = 0
+    normalisedFuture = []
+    for i in similarPatterns:
+        normalised = []
+        
+        ratio = float(currentPattern[-1] / i[0][-(1 + futureDistance)])
+        normalisedFuture.append(futurePrices[count] * ratio)
+        #print(normalisedFuture)
+        xList.append(25)
+        for x in range(len(currentPattern) + futureDistance):
+            normalised.append(i[0][x] * ratio)
+        pyplot.plot(normalised)
+        count += 1
+    pyplot.plot(currentPattern,linewidth = 5.0)
+    '''for x in range(len(currentPattern)):
+            normalisedFuture.append(futurePrices[x] * ratio)
+            xList.append(17)'''
+    #yList = futureResults(similarPatterns)
+    yList = normalisedFuture
+    print(yList)
+    print(int(len(yList)))
+    print(xList)
+    print(int(len(xList)))
+    pyplot.scatter(xList, yList)
+    pyplot.scatter(25,historicData[-275],color = 'red')
+    pyplot.show()
 
 pastPatterns = generatePastPatterns(len(currentPattern), historicData)
 similarPatterns = []
 from shapeComparison import comparePattern
 for i in pastPatterns:
-    x = x
+    #print('Past pattern : '+str(i))
+    if comparePattern(currentPattern,i[0],1) > 35:
+        similarPatterns.append(i)
+futurePrices = []
+def futureResults(similarPatterns):
+    futureResults = []
+    fururePrices = []
+    for i in similarPatterns:
+        futurePrice = historicData[i[1] + len(currentPattern) + futureDistance]
+        futurePrices.append(futurePrice)
+        futurePercentage = percentageChange(historicData[i[1] + len(currentPattern)],futurePrice)
+        futureResults.append(futurePercentage)
+    return futureResults
+print('Found ' +str(len(similarPatterns)) + ' similar patterns')
+
+print('Done')
+
+print(futureResults(similarPatterns))
+print(reduce(lambda x, y: x + y, futureResults(similarPatterns)) / float(len(futureResults(similarPatterns))))
+graphSimilar()
--- shapeComparison.py
+++ shapeComparison.py
@@ -1,7 +1,9 @@
 from functools import reduce
 
 def percentageChange(dataFrom, dataTo):#Calculates exactly what you'd think
-    return ((dataTo - dataFrom)/abs(dataFrom))*100.0
+    if dataFrom == 0:
+        return 0
+    return ((float(dataTo) - float(dataFrom))/abs(float(dataFrom)))*100.0
 
 def percentageSteps(data):                                              #creates a list of percentage changes between each step in the data,
                                                                         #list generated will be shorter by 1 that original data
@@ -16,12 +18,13 @@ def logarithmicChange(dataFrom,dataTo):
 def comparePattern(pattern1,pattern2,methodNumber):
     if methodNumber == 1.0:                                             #percentage change comparison
         pattern1Steps = percentageSteps(pattern1)                       #Change to data to percentage steps
-        print(pattern1Steps)
+        #print('Pattern 1 Steps ' + str(pattern1Steps))
         pattern2Steps = percentageSteps(pattern2)
-        print(pattern2Steps)
+        #print('Pattern 2 steps ' + str(pattern2Steps))
         differencesInChanges = []
         for i in range(len(pattern1Steps)):
+            #print(100- abs(percentageChange(pattern1Steps[i],pattern2Steps[i])))
             differencesInChanges.append(100- abs(percentageChange(pattern1Steps[i],pattern2Steps[i])))
-        print(differencesInChanges)
+        #print(differencesInChanges)
         averageDifference = reduce(lambda x, y: x + y, differencesInChanges) / float(len(differencesInChanges))
         return averageDifference
